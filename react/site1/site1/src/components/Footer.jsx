06c8cf10f429fc3cc13d71e31ddd95b79d512a21
AngelRafaelTopsapp <rafael.souza@topsapp.com.br>
2024-09-17 09:51:55 -0400

    Delete react/site1/site1 directory

--- react/site1/site1/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import "../";
-function Footer() {
-  return (
-    <footer>
-        <p className="fontFooter">COPYRIGHT 2022 - TODOS OS DIREITOS RESERVADOS A AMIGOS TOP</p>
-    </footer>
-    );
-  }
-  export default Footer;
